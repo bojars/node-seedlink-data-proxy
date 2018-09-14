ff183c6573aaac5205a3c492290812734f195983
davis <2466545+bojars@users.noreply.github.com>
2018-09-14 15:55:17 +0300

    Updated test.js to close socket when shutdown

--- test.js
+++ test.js
@@ -1,11 +1,43 @@
 var osc = require('node-osc')
 
+var connected = false
 var client = new osc.Client('127.0.0.1', 9002)
-client.send('/subscribe', 'NL.HGN', function () {
-  client.kill()
+client.send('/subscribe', 'BHZ.ALL', function () {
+  connected = true
 })
 
 var oscServer = new osc.Server(9001, '0.0.0.0')
 oscServer.on('message', function (msg, rinfo) {
-  console.log(msg)
+  const path = msg.shift()
+  const ident = msg.shift()
+  const data = msg
+
+  console.log(path, ident, data.length)
 })
+
+var isStopping = false
+function shutdown () {
+  if (isStopping) {
+    return
+  } else {
+    isStopping = true
+  }
+
+  console.info('Got SIGTERM. Graceful shutdown start', new Date().toISOString())
+
+  if (connected) {
+    client.send('/unsubscribe', 'BHZ.ALL', function () {
+      process.exit()
+    })
+  } else {
+    process.exit()
+  }
+
+  setTimeout(() => {
+    console.log('Forcing shutdown after 30sec')
+    process.exit()
+  }, 30 * 1000)
+}
+
+process.once('SIGTERM', shutdown)
+process.once('SIGINT', shutdown)
